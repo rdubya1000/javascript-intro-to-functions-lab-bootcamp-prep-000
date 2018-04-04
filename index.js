362e1a8e67b85f54501d0a61dd0feff9a2f348ec
rwofford0@gmail.com <rwofford0@gmail.com>
2018-04-04 19:23:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,17 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-
+function sayHiToGrandma(string){
+  if (string.toUpperCase()===string) {
+    return "YES INDEED"
+  
+  } else if (string.toLowerCase()===string){
+    return "I can\'t hear you."
+      
+  }else if (string==="I love you, Grandma."){
+    return "I love you, too"
+  }
+  
+  
+  
+}
